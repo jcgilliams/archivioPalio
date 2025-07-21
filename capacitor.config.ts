c460aed876f77773369573b03d4adfa37029564e
JC Gilliams <jc.gilliams@gmail.com>
2025-07-21 18:07:48 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'archivioPalio',
+  webDir: 'www'
+};
+
+export default config;
